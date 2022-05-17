cddb93d50c75b89b2e7c6ca1f865a1df3b27a403
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-05-17 11:51:35 -0500

    -- Observation -- Got some of the images to be restyled to an acceptable standard, gonna refactor this probably in the future depending on the display images used.

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css';
 
 // images
-import menuBar from '../public/menuBar.svg'
+import giveDisplay from '../public/giveDisplay.jpeg';
+import jhamDisplay from '../public/jhamDisplay.jpeg';
+
 
 // import home JSON object
 import home from '../src/assets/home.json';
@@ -20,6 +22,20 @@ export default function Home() {
       <NavBar />
 
       <div className={styles.headerDivider}></div>
+
+      <CaseCard 
+        imageSrc={giveDisplay}
+        imageAlt={"This is the display image for the Givelify Case Study"}
+        title={"Designing a magical donation experience"}
+        year={"2021-2022"}  
+      />
+      
+      <CaseCard 
+        imageSrc={jhamDisplay}
+        imageAlt={"This is the display image for the Jham Case Study"}
+        title={"Building a convenient IT helpdesk experience"}
+        year={"2020-2021"}  
+      />
 
     </div>
   )
--- src/components/CaseCard.js
+++ src/components/CaseCard.js
@@ -6,8 +6,8 @@ const CaseCard = ({ imageSrc, imageAlt, title, year }) => {
   
   return (
     <div className={styles.caseCardContainer}>
-      <div className={styles.caseCardImage}>
-        <Image src={imageSrc} alt={imageAlt}/>
+      <div className={styles.caseCardImageContainer}>
+        <Image src={imageSrc} alt={imageAlt} height="1600" layout="responsive" objectFit="cover"/>
       </div>
       <div className={styles.caseCardContent}>
         <h5>{title}</h5>
