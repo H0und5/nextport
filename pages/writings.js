c94fb450cd6825592134b6852ea0531dcb584bb9
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-05-18 12:49:50 -0500

    -- Implementation -- Updated and cleaned up the routes to deploy to vercel

---
+++ pages/writings.js
@@ -0,0 +1,19 @@
+// import Head from 'next/head'
+import styles from '../styles/Home.module.css';
+
+// components
+import NavBar from '../src/components/NavBar';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+
+      <NavBar />
+
+      <div className={styles.headerDivider}></div>
+
+
+
+    </div>
+  )
+}
