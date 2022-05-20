4797da52fab684ba231d85d95a982ec46d579a6b
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-05-20 12:21:13 -0500

    -- Renovation -- Changed a bit of the styling for the MobileBio.

--- src/components/MobileBio.js
+++ src/components/MobileBio.js
@@ -7,7 +7,6 @@ const MobileBio = () => {
   return (
     <div className={styles.mobileBioContainer}>
       Digital product designer <span className={styles.mobileBioGreyed}> located in Dallas, currently building interactive experiences at a fin-tech startup.
-      <SmallDivider />
       Open to opportunities.</span> 
     </div>
   )
