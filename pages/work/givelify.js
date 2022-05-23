05e09be5a2c7c58a322dbfcdbd2f526c935c9534
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-05-23 17:07:06 -0500

    -- Initialization -- Testing this Image shit

--- pages/work/givelify.js
+++ pages/work/givelify.js
@@ -1,18 +1,24 @@
 // import Head from 'next/head'
 import styles from "../../styles/Home.module.css";
 
-import ImageCaseCover from "../../src/components/ImageCovers/ImageCaseCover";
 import CaseCard from "../../src/components/Cards/CaseCard";
 
 import giveDisplay from "../../public/giveDisplay.jpeg";
+import Image from "next/image";
+import SmallDivider from "../../src/components/Dividers/SmallDivider";
+import BigDivider from "../../src/components/Dividers/BigDivider";
 
 // components
 
 export default function Home() {
   return (
     <div className={styles.container}>
+
+      <div className={styles.headerDivider}></div>
+
+      <BigDivider />
       
-      <ImageCaseCover imageSrc={giveDisplay} alt={"this is a start"}/>
+      <Image src={giveDisplay} alt="example image" />
       
       <div className={styles.headerDivider}></div>
 
