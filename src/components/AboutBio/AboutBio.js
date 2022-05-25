8e650a9fb5c3bfc17d1d64b625a91da9cda711e6
H0und5 <84218198+H0und5@users.noreply.github.com>
2022-05-24 19:12:03 -0500

    -- Renovation -- Updated some more mobile and desktop styling for the case Study layout shit

---
+++ src/components/AboutBio/AboutBio.js
@@ -0,0 +1,14 @@
+import styles from './AboutBio.module.css';
+
+const AboutBio = () => {
+
+  return (
+    <div>
+      <h1>
+        Digital product designer in Dallas, TX
+      </h1>
+    </div>
+  )
+}
+
+export default AboutBio;
